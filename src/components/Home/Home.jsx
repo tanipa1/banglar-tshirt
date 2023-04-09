1b2a08df56dc55d3b9fe81d8a90645ba2574601f
tanipa1 <tania35.291@gmail.com>
2023-04-09 22:50:48 +0600

    Product List

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,11 +1,32 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useLoaderData } from 'react-router-dom';
+import Tshirt from '../Tshirt/Tshirt';
+import Cart from '../Cart/Cart';
+import './Home.css';
 
 const Home = () => {
     const tshirts = useLoaderData()
+    const [cart, setCart] = useState([]);
+
+    const handleAddToCart = tshirt =>{
+
+    }
+
     return (
-        <div>
-            <h1>This is Home: {tshirts.length}</h1>
+        <div className='home-container'>
+            <div className='tshirts-container'>
+            {
+                tshirts.map(tshirt => 
+                <Tshirt
+                    key={tshirt._id}
+                    tshirt={tshirt}
+                    handleAddToCart={handleAddToCart}
+                ></Tshirt>)
+            }
+            </div>
+            <div className="cart-container">
+                <Cart></Cart>
+            </div>
         </div>
     );
 };
